e1e47d8393a4922ba32824242710bd21f69c583f
Selina Le Khac <selinalekhac@Selinas-MacBook-Pro.local>
2019-10-23 23:55:39 +1100

    recycler view added, however still need to add adapter and database

---
+++ app/src/main/java/com/example/myapplicationtask2/fragment_food_item_recycler.java
@@ -0,0 +1,31 @@
+package com.example.myapplicationtask2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_food_item_recycler extends Fragment {
+
+
+    public fragment_food_item_recycler() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_food_item_recycler, container, false);
+    }
+
+}
